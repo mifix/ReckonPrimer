79913e27748121d191af8782b67f122a7036017d
Walther Neuper <neuper@neuper.(none)>
2009-11-17 12:30:25 +0100

    setup of RP.tests, intermediate test

--- ReckonPrimer.tests/timesdiv/test_generate_calc.py
+++ ReckonPrimer.tests/timesdiv/test_generate_calc.py
@@ -5,7 +5,7 @@ print("===== ReckonPrimer.tests/timesdiv/test_generate_calc.py ==============")
 
 #WN091116 Exercise <10> does not generate calculations with "1+2  <" OFF
 # Error Traceback ends with:
-# File "/home/neuper/Activities/ReckonPrimer.activity/passten.py", line 53, in format
+# File "/home/neuper/Activities/ReckonPrimer.activity/passten.py", line 53, in format   
 #   return Exercise.format_addsub_simp((cs, linepos))
 # TypeError: unbound method format_addsub_simp() must be called with Exercise instance as first argument (got tuple instance instead)
 
